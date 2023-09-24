f31fea28dd22587496d76d18f36e9f8e57a74f75
abdullah <abdullahiqbal599@gmail.com>
2023-09-24 15:34:54 +0500

    first project

---
+++ src/lms/Lms.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lms;
+import java.sql.*;
+import project.ConnectionProvider;
+/**
+ *
+ * @author Abdullah
+ */
+public class Lms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        Connection con = ConnectionProvider.getCon();
+//        System.out.println(con);
+String f1 = "453";
+        String text = f1;
+int xaxis1 = Integer.parseInt(text);
+     System.out.println(xaxis1);   
+    }
+    
+}
